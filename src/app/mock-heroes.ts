09ac1ee9f195e2f08d860e518828722bfb5d7448
RaulImanol <201822055@tese.edu.mx>
2021-03-23 13:16:27 -0600

    Se realizaron cambios para agregar los super heroes en lista dinamica

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import {Hero} from "./hero";
+
+export const HEROES: Hero[]=[
+    {id: 11, name:"Superman", edad:30, poder:"Todos", identidad: "Peter Parker", universo:"DC"},
+    {id: 12, name:"Batman", edad:30, poder:"Todos", identidad: "Peter Parker", universo:"DC" },
+    {id: 13, name:"Hulk",edad:30, poder:"Todos", identidad: "Peter Parker", universo:"DC"},
+    {id: 14, name:"Ironman", edad:30, poder:"Todos", identidad: "Peter Parker", universo:"DC"},
+    {id: 15, name:"Thor", edad:30, poder:"Todos", identidad: "Peter Parker", universo:"DC"},
+    {id: 16, name:"Spiderman", edad:30, poder:"Todos", identidad: "Peter Parker", universo:"DC"},
+    {id: 17, name:"Black Widow", edad:30, poder:"Todos", identidad: "Peter Parker", universo:"DC"},
+    {id: 18, name:"Flash", edad:30, poder:"Todos", identidad: "Peter Parker", universo:"DC"},
+    {id: 19, name:"Bruja escarlata", edad:30, poder:"Todos", identidad: "Peter Parker", universo:"DC"},
+    {id: 20, name:"Wonder Woman", edad:30, poder:"Todos", identidad: "Peter Parker", universo:"DC"},
+
+];
